13bc35da24d9181b6d678e510a1eba52e8aa8f74
Stefan Kendall <stefankendall@gmail.com>
2011-11-06 16:14:20 -0500

    Fixed application initialization process to not hang at black screen.

--- js/wendler.js
+++ js/wendler.js
@@ -1,25 +1,31 @@
 "use strict";
-Ext.setup({
+Ext.ns('wendler');
+var wendlerApp = new Ext.Application({
     tabletStartupScreen: 'tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
     icon: 'apple-touch-icon.png',
     glossOnIcon: false,
 
-    onReady: function() {
-        new Ext.TabPanel({
-            id: 'main-tab-panel',
-            fullscreen: true,
-            sortable: true,
-            tabBar: {
-                dock: 'bottom',
-                layout: { pack: 'center' }
-            },
-            items: [
-                new wendler.views.LiftSchedule(),
-                new wendler.views.Maxes(),
-                new wendler.views.OneRepMaxCalculator(),
-                new wendler.views.Settings()
-            ]
-        });
-    }
+    viewport:null,
+    launched:false,
+    launch: function() {
+        if (!this.launched)
+            return this.launched = true;
+        this.viewport = new this.Viewport();
+    },
+    Viewport: Ext.extend(Ext.TabPanel, {
+        id: 'main-tab-panel',
+        fullscreen: true,
+        sortable: true,
+        tabBar: {
+            dock: 'bottom',
+            layout: { pack: 'center' }
+        },
+        items: [
+            new wendler.views.LiftSchedule(),
+            new wendler.views.Maxes(),
+            new wendler.views.OneRepMaxCalculator(),
+            new wendler.views.Settings()
+        ]
+    })
 });
