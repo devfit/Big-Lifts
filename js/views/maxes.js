7908717213faae122e02aebcb86c91a7997192cb
Stefan Kendall <stefankendall@gmail.com>
2011-11-07 23:24:22 -0500

    Starting on UI work to enable adding lifts

--- js/views/maxes.js
+++ js/views/maxes.js
@@ -23,6 +23,24 @@ wendler.maxes.controller.liftValuesChanged = function(el, newValue) {
     lift.save();
 };
 
+wendler.maxes.controller.addLiftButtonPressed = function() {
+    Ext.getCmp('maxes-form-items').items.each(function(input) {
+        input.disable();
+    });
+
+    Ext.getCmp('add-lift-button').hide();
+    Ext.getCmp('add-lift-done-button').show();
+};
+
+wendler.maxes.controller.addLiftDoneButtonPressed = function() {
+    Ext.getCmp('maxes-form-items').items.each(function(input) {
+        input.enable();
+    });
+
+    Ext.getCmp('add-lift-button').show();
+    Ext.getCmp('add-lift-done-button').hide();
+};
+
 wendler.views.Maxes = Ext.extend(Ext.Panel, {
     id: 'maxes-panel',
     title: 'Maxes',
@@ -35,7 +53,23 @@ wendler.views.Maxes = Ext.extend(Ext.Panel, {
                 {
                     xtype: 'toolbar',
                     dock: 'top',
-                    title: 'Maxes'
+                    title: 'Maxes',
+                    items: [
+                        {xtype: 'spacer'},
+                        {
+                            id: 'add-lift-button',
+                            ui: 'action',
+                            text: 'Add Lift',
+                            handler: wendler.maxes.controller.addLiftButtonPressed
+                        },
+                        {
+                            id: 'add-lift-done-button',
+                            ui: 'action',
+                            text: 'Done',
+                            hidden:true,
+                            handler: wendler.maxes.controller.addLiftDoneButtonPressed
+                        }
+                    ]
                 }
             ],
             items: [
