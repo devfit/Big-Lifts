dcc87ea5126d1d197cea66c4460c9dfd4bc9fcf7
Stefan Kendall <stefankendall@gmail.com>
2011-12-01 00:25:04 -0500

    Added simple tests for the lift schedule tab.

--- selenium-test/spec/functional/liftSchedule_spec.rb
+++ selenium-test/spec/functional/liftSchedule_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
 describe 'The lift schedule tab' do
-  before(:each) do
+  before(:all) do
     @driver = Selenium::WebDriver.for :chrome
+    @driver.navigate.to 'http://localhost:3000/'
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+    @wait.until { @driver.find_element(:id => "lift-selector") }
   end
 
-  it 'Shows the lift list', :js => true do
-    @driver.navigate.to 'http://localhost:3000/index.html'
+  after(:all) do
+    @driver.quit
+  end
+
+  it 'includes all built-in lifts' do
     liftList = @driver.find_element(:id => 'lift-selector')
-    puts liftList.text
+    liftList.text.should include('Squat', 'Deadlift', 'Press', 'Bench')
+  end
+
+  it 'Shows warmup and work sets' do
+    squatListItem = @driver.find_element(:class => 'x-list-item', :text => 'Squat')
+    squatListItem.click
+    @wait.until { @driver.find_element(:id => 'lift-template') }
+    liftTemplate = @driver.find_element(:id => 'lift-template')
+    liftTemplate.find_elements(:class => 'lift-row').length.should == 6
   end
 end
