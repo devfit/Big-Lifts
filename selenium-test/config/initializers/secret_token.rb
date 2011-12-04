90a38ef8dfc694c9762612f745929449df6aba9d
Stefan Kendall <stefankendall@gmail.com>
2011-12-04 11:52:26 -0500

    Removing rails experiment

--- selenium-test/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SeleniumTest::Application.config.secret_token = 'b3d5a12357ae00141cbe6a8b6de0dd7a022611c822bb9cba5259e6cfb9ce36de8fa98784419f0a77357a9373a8518db28534bace95c4d4b9c3d50b507e6ee0a6'
