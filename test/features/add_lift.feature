d65aa90055604207331e2c26ec86ce16f63663f9
Stefan Kendall <stefankendall@gmail.com>
2011-12-06 22:52:15 -0500

    Filled out most of spec, modified feature to reflect actual user behavior

--- test/features/add_lift.feature
+++ test/features/add_lift.feature
@@ -5,10 +5,12 @@ Feature: adding lifts
 
   Scenario: add a new lift
     When I add a new lift named "Clean and Jerk" with max 200
+    And When I close the edit lifts screen
     Then "Clean and Jerk" is added to the edit lifts screen
     And "Clean and Jerk" is added to the lift schedule
 
   Scenario: add a new lift with an invalid name
     When I add a new lift named "222" with max 300
     Then I see an error with message "Invalid lift name"
+    And I close the add lift screen
     And "222" is not added to the edit lifts screen
--- test/features/step_definition/add_lift.rb
+++ test/features/step_definition/add_lift.rb
@@ -11,6 +11,8 @@ When /^I add a new lift named "([^"]*)" with max (\d+)$/ do |lift, max|
   addLiftButton.click
 
   @wait.until { @driver.find_elements(:id => 'add-lift-new-name') != [] }
+  sleep 0.25
+
   nameInput = @driver.find_element(:name => 'add-lift-new-name')
   nameInput.send_keys lift
   maxInput = @driver.find_element(:name => 'add-lift-new-max')
@@ -19,16 +21,40 @@ When /^I add a new lift named "([^"]*)" with max (\d+)$/ do |lift, max|
   @driver.find_element(:id => 'add-lift-done-button').click
 end
 
+Then /^When I close the edit lifts screen$/ do
+  edit_lifts_done_button = @driver.find_element(:id => 'edit-lifts-done-button')
+  @wait.until { edit_lifts_done_button.displayed? }
+  sleep 0.5
+  edit_lifts_done_button.click
+end
+
 Then /^"([^"]*)" is ([\w ]+)?added to the edit lifts screen$/ do |lift, invertCheck|
-  puts invertCheck == "not"
+  liftShouldBeAdded = invertCheck == nil
+  @main_navigation.navigate_to(:lift_editor)
+  editLiftsButton = @driver.find_element(:id => 'edit-lifts-button')
+  @wait.until { editLiftsButton.displayed? }
+
+  liftList = @driver.find_element(:id => 'maxes-form-items')
+  liftIsShown = liftList.text.include? lift
+
+  liftIsShown.should == liftShouldBeAdded
 end
 
 Then /^"([^"]*)" is ([\w ]+)?added to the lift schedule$/ do |lift, invertCheck|
-  puts invertCheck == "not"
+  if (invertCheck != nil)
+
+  end
 end
 
 Then /^I see an error with message "([^"]*)"$/ do |message|
-  @wait.until { @driver.find_elements( :class => 'x-msgbox-body' ) != [] }
-  @driver.find_element( :class => 'x-msgbox-body' ).text.should == message
+  @wait.until { @driver.find_elements(:class => 'x-msgbox-body') != [] }
+  @driver.find_element(:class => 'x-msgbox-body').text.should == message
+  @driver.find_element(:class => 'x-msgbox').find_element(:class => 'x-button').click
+end
+
+Then /^I close the add lift screen$/ do
+  @driver.find_element(:id => 'add-lift-cancel-button').click
+  sleep 0.25
+  @driver.find_element(:id => 'edit-lifts-done-button').click
 end
 
