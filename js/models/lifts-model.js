c8976e82b3b1c45b0e30daf2407ed1524c621baf
Stefan Kendall <stefankendall@gmail.com>
2011-12-18 01:10:25 -0500

    Added marking of lifts completed.

--- js/models/lifts-model.js
+++ js/models/lifts-model.js
@@ -2,7 +2,7 @@
 Ext.ns('wendler.stores.lifts', 'wendler.defaults');
 Ext.ns('wendler.stores.migrations', 'wendler.models.Lift');
 
-wendler.models.Lift.uniquePropertyNameValidation = function(propertyName) {
+wendler.models.Lift.uniquePropertyNameValidation = function (propertyName) {
     var liftIsUnique = wendler.stores.lifts.Lifts.find('propertyName', propertyName, 0,
         false, true, true) == -1;
 
@@ -10,35 +10,35 @@ wendler.models.Lift.uniquePropertyNameValidation = function(propertyName) {
 };
 
 Ext.regModel('Lift', {
-    fields: [
-        {name: 'id', type: 'integer'},
-        {name: 'name', type: 'string'},
-        {name: 'propertyName', type: 'string'},
-        {name: 'max', type: 'integer'}
+    fields:[
+        {name:'id', type:'integer'},
+        {name:'name', type:'string'},
+        {name:'propertyName', type:'string'},
+        {name:'max', type:'integer'}
     ],
     validations:[
-        {field:'propertyName', type: 'custom', message: 'nonunique',
-            fn: wendler.models.Lift.uniquePropertyNameValidation},
-        {field: 'propertyName', type:'presence'},
-        {field: 'max', type: 'format', matcher:/^\d\d*$/, message: 'Must enter a max'},
-        {field: 'max', type: 'presence'}
+        {field:'propertyName', type:'custom', message:'nonunique',
+            fn:wendler.models.Lift.uniquePropertyNameValidation},
+        {field:'propertyName', type:'presence'},
+        {field:'max', type:'format', matcher:/^\d\d*$/, message:'Must enter a max'},
+        {field:'max', type:'presence'}
     ],
-    proxy: {
-        type: 'localstorage',
-        id: 'lift-proxy'
+    proxy:{
+        type:'localstorage',
+        id:'lift-proxy'
     }
 });
 
 
 wendler.defaults.lifts = [
-    Ext.ModelMgr.create({name: 'Squat', max: 200, propertyName: 'squat'}, 'Lift'),
-    Ext.ModelMgr.create({name: 'Deadlift', max: 300, propertyName: 'deadlift'}, 'Lift'),
-    Ext.ModelMgr.create({name: 'Press', max: 150, propertyName: 'press'}, 'Lift'),
-    Ext.ModelMgr.create({name: 'Bench', max: 175, propertyName:'bench'}, 'Lift')
+    Ext.ModelMgr.create({name:'Squat', max:200, propertyName:'squat'}, 'Lift'),
+    Ext.ModelMgr.create({name:'Deadlift', max:300, propertyName:'deadlift'}, 'Lift'),
+    Ext.ModelMgr.create({name:'Press', max:150, propertyName:'press'}, 'Lift'),
+    Ext.ModelMgr.create({name:'Bench', max:175, propertyName:'bench'}, 'Lift')
 ];
 
-wendler.stores.migrations.liftModelMigration = function() {
-    wendler.stores.lifts.Lifts.each(function(r) {
+wendler.stores.migrations.liftModelMigration = function () {
+    wendler.stores.lifts.Lifts.each(function (r) {
         if (!r.data.propertyName) {
             var propertyName = wendler.models.Lift.sanitizePropertyName(r.data.name);
             r.set('propertyName', propertyName);
@@ -47,14 +47,14 @@ wendler.stores.migrations.liftModelMigration = function() {
     });
 };
 
-wendler.models.Lift.sanitizePropertyName = function(propertyName) {
+wendler.models.Lift.sanitizePropertyName = function (propertyName) {
     return propertyName.toLowerCase().replace(/[^a-z]/g, '');
 };
 
 wendler.stores.lifts.Lifts = new Ext.data.Store({
-    model: 'Lift',
-    listeners: {
-        load: function() {
+    model:'Lift',
+    listeners:{
+        load:function () {
             if (this.getCount() == 0) {
                 this.add(wendler.defaults.lifts);
                 this.sync();
@@ -62,60 +62,105 @@ wendler.stores.lifts.Lifts = new Ext.data.Store({
             wendler.stores.migrations.liftModelMigration();
         }
     },
-    autoLoad: true,
-    autoSave: true
+    autoLoad:true,
+    autoSave:true
 });
 
 Ext.regModel('LiftProgression', {
-    fields: [
-        {name: 'id', type: 'integer'},
-        {name: 'week', type: 'integer'},
-        {name: 'set', type: 'integer'},
-        {name: 'reps', type: 'integer'},
-        {name: 'percentage', type: 'integer'}
+    fields:[
+        {name:'id', type:'integer'},
+        {name:'week', type:'integer'},
+        {name:'set', type:'integer'},
+        {name:'reps', type:'integer'},
+        {name:'percentage', type:'integer'}
     ],
-    proxy: {
-        type: 'localstorage',
-        id: 'lift-percentages-proxy'
+    proxy:{
+        type:'localstorage',
+        id:'lift-percentages-proxy'
     }
 });
 
 wendler.defaults.liftProgression = [
-    Ext.ModelMgr.create({week: 1, set: 1, reps: 5, percentage: 40}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 1, set: 2, reps: 5, percentage: 50}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 1, set: 3, reps: 3, percentage: 60}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 1, set: 4, reps: 5, percentage: 65}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 1, set: 5, reps: 5, percentage: 75}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 1, set: 6, reps: 5, percentage: 85}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 2, set: 1, reps: 5, percentage: 40}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 2, set: 2, reps: 5, percentage: 50}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 2, set: 3, reps: 3, percentage: 60}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 2, set: 4, reps: 3, percentage: 70}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 2, set: 5, reps: 3, percentage: 80}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 2, set: 6, reps: 3, percentage: 90}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 3, set: 1, reps: 5, percentage: 40}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 3, set: 2, reps: 5, percentage: 50}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 3, set: 3, reps: 3, percentage: 60}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 3, set: 4, reps: 5, percentage: 75}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 3, set: 5, reps: 3, percentage: 85}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 3, set: 6, reps: 1, percentage: 95}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 4, set: 1, reps: 5, percentage: 40}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 4, set: 2, reps: 5, percentage: 50}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 4, set: 3, reps: 3, percentage: 60}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 4, set: 4, reps: 5, percentage: 60}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 4, set: 5, reps: 5, percentage: 65}, 'LiftProgression'),
-    Ext.ModelMgr.create({week: 4, set: 6, reps: 5, percentage: 70}, 'LiftProgression')
+    Ext.ModelMgr.create({week:1, set:1, reps:5, percentage:40}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:1, set:2, reps:5, percentage:50}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:1, set:3, reps:3, percentage:60}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:1, set:4, reps:5, percentage:65}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:1, set:5, reps:5, percentage:75}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:1, set:6, reps:5, percentage:85}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:2, set:1, reps:5, percentage:40}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:2, set:2, reps:5, percentage:50}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:2, set:3, reps:3, percentage:60}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:2, set:4, reps:3, percentage:70}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:2, set:5, reps:3, percentage:80}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:2, set:6, reps:3, percentage:90}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:3, set:1, reps:5, percentage:40}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:3, set:2, reps:5, percentage:50}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:3, set:3, reps:3, percentage:60}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:3, set:4, reps:5, percentage:75}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:3, set:5, reps:3, percentage:85}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:3, set:6, reps:1, percentage:95}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:4, set:1, reps:5, percentage:40}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:4, set:2, reps:5, percentage:50}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:4, set:3, reps:3, percentage:60}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:4, set:4, reps:5, percentage:60}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:4, set:5, reps:5, percentage:65}, 'LiftProgression'),
+    Ext.ModelMgr.create({week:4, set:6, reps:5, percentage:70}, 'LiftProgression')
 ];
 wendler.stores.lifts.LiftProgression = new Ext.data.Store({
-    model: 'LiftProgression',
-    listeners: {
-        load: function() {
+    model:'LiftProgression',
+    listeners:{
+        load:function () {
             if (this.getCount() == 0) {
                 this.add(wendler.defaults.liftProgression);
                 this.sync();
             }
         }
     },
-    autoLoad: true,
-    autoSave: true
+    autoLoad:true,
+    autoSave:true
 });
+
+Ext.regModel('LiftCompletion', {
+    fields:[
+        {name:'id', type:'integer'},
+        {name:'liftPropertyName', type:'string'},
+        {name:'week', type:'integer'},
+        {name:'completed', type:'boolean'}
+    ],
+    proxy:{
+        type:'localstorage',
+        id:'lift-completion-proxy'
+    }
+});
+wendler.stores.lifts.LiftCompletion = new Ext.data.Store({
+    model:'LiftCompletion',
+    listeners:{
+        load:function () {
+            if (this.getCount() == 0) {
+                wendler.stores.migrations.liftCompletionMigration();
+            }
+        }
+    },
+    autoLoad:true,
+    autoSave:true
+});
+wendler.stores.lifts.findLiftCompletionByPropertyAndWeek = function(liftPropertyName, week){
+    var completionIndex = wendler.stores.lifts.LiftCompletion.findBy(function (r) {
+        return r.get('liftPropertyName') === liftPropertyName && r.get('week') === week;
+    });
+    return wendler.stores.lifts.LiftCompletion.getAt(completionIndex);
+};
+
+wendler.stores.migrations.liftCompletionMigration = function () {
+    for (var i = 0; i < wendler.stores.lifts.Lifts.getCount(); i++) {
+        var liftPropertyName = wendler.stores.lifts.Lifts.getAt(i).get('propertyName');
+        for (var week = 1; week <= 4; week++) {
+            wendler.stores.lifts.LiftCompletion.add(
+                {liftPropertyName:liftPropertyName, week:week, completed:false});
+        }
+    }
+
+    wendler.stores.lifts.LiftCompletion.sync();
+};
+
+
--- js/views/liftSchedule.js
+++ js/views/liftSchedule.js
@@ -2,7 +2,7 @@
 Ext.ns('wendler.views', 'wendler.liftSchedule.controller');
 
 wendler.liftSchedule.currentShowingMax = -1;
-wendler.liftSchedule.currentLiftName = null;
+wendler.liftSchedule.currentLiftProperty = null;
 wendler.liftSchedule.currentWeek = -1;
 
 wendler.liftSchedule.controller.formatLiftWeight = function (max, percentage) {
@@ -26,8 +26,8 @@ wendler.liftSchedule.controller.switchLiftWeekForComponent = function (newcard) 
 wendler.liftSchedule.controller.updateLiftValues = function () {
     var showWarmupSets = wendler.stores.Settings.first().data['show-warmup-sets'];
 
-    if (wendler.liftSchedule.currentLiftName) {
-        wendler.liftSchedule.currentShowingMax = wendler.stores.lifts.Lifts.findRecord('name', wendler.liftSchedule.currentLiftName).data.max;
+    if (wendler.liftSchedule.currentLiftProperty) {
+        wendler.liftSchedule.currentShowingMax = wendler.stores.lifts.Lifts.findRecord('propertyName', wendler.liftSchedule.currentLiftProperty).data.max;
         wendler.stores.lifts.LiftProgression.clearFilter();
         wendler.stores.lifts.LiftProgression.filter("week", wendler.liftSchedule.currentWeek);
 
@@ -44,7 +44,7 @@ wendler.liftSchedule.controller.viewLift = function (view, index) {
     var liftName = record.get('name');
     Ext.getCmp('lift-template-toolbar').setTitle(liftName);
 
-    wendler.liftSchedule.currentLiftName = liftName;
+    wendler.liftSchedule.currentLiftProperty = record.get('propertyName');
 
     var liftSelector = Ext.getCmp('lift-selector');
     wendler.liftSchedule.controller.switchLiftWeekForComponent(liftSelector.getActiveItem());
@@ -72,18 +72,68 @@ wendler.liftSchedule.controller.setupDoneIcons = function () {
     for (var weekIndex = 0; weekIndex < liftLists.length; weekIndex++) {
         var liftList = liftLists[weekIndex];
         var listItems = liftList.getEl().query('.x-list-item');
-        for (var listItemIndex in listItems) {
+        for (var listItemIndex = 0; listItemIndex < listItems.length; listItemIndex++) {
             var listItem = listItems[listItemIndex];
-            if (wendler.liftSchedule.controller.liftHasBeenCompleted(weekIndex, listItemIndex)) {
-                 Ext.get(listItem).addCls('done');
+            if (wendler.liftSchedule.controller.liftHasBeenCompleted(weekIndex + 1, listItemIndex)) {
+                Ext.get(listItem).addCls('done');
+            }
+            else {
+                Ext.get(listItem).removeCls('done');
             }
         }
     }
 };
 
-wendler.liftSchedule.controller.liftHasBeenCompleted = function (week, lift) {
-    //TODO: Add logic.
-    return lift % 2 == 0;
+wendler.liftSchedule.controller.liftHasBeenCompleted = function (week, liftIndex) {
+    var liftPropertyName = wendler.stores.lifts.Lifts.getAt(liftIndex).get('propertyName');
+    return wendler.stores.lifts.findLiftCompletionByPropertyAndWeek(liftPropertyName, week).get('completed');
+};
+
+wendler.liftSchedule.controller.setupLiftCompleteToggle = function () {
+    var completed = wendler.stores.lifts.findLiftCompletionByPropertyAndWeek(wendler.liftSchedule.currentLiftProperty,
+        wendler.liftSchedule.currentWeek).get('completed');
+
+    var unmarkButton = Ext.getCmp('unmark-lift-completed-button');
+    var markButton = Ext.getCmp('mark-lift-completed-button');
+    if (completed) {
+        unmarkButton.show();
+        markButton.hide();
+    }
+    else {
+        markButton.show();
+        unmarkButton.hide();
+    }
+};
+
+wendler.liftSchedule.controller.markHandling = function (shouldMark) {
+    var liftCompletion = wendler.stores.lifts.findLiftCompletionByPropertyAndWeek(wendler.liftSchedule.currentLiftProperty, wendler.liftSchedule.currentWeek);
+    liftCompletion.set('completed', shouldMark);
+    liftCompletion.save();
+
+    var unmarkButton = Ext.getCmp('unmark-lift-completed-button');
+    var markButton = Ext.getCmp('mark-lift-completed-button');
+
+    if (shouldMark) {
+        unmarkButton.show();
+        markButton.hide();
+    }
+    else {
+        unmarkButton.hide();
+        markButton.show();
+    }
+    wendler.liftSchedule.controller.setupDoneIcons();
+};
+
+wendler.liftSchedule.controller.markLiftCompleted = function () {
+    wendler.liftSchedule.controller.markHandling(true);
+};
+
+wendler.liftSchedule.controller.unmarkLiftCompleted = function () {
+    wendler.liftSchedule.controller.markHandling(false);
+};
+
+wendler.liftSchedule.controller.returnToLiftSelect = function () {
+    Ext.getCmp('lift-schedule').setActiveItem(Ext.getCmp('lift-selector'), {type:'slide', direction:'right'});
 };
 
 wendler.liftSchedule.liftTemplate = {
@@ -98,6 +148,9 @@ wendler.liftSchedule.liftTemplate = {
             itemTpl:'<p><span class="reps">{reps}x</span> <span>{[wendler.liftSchedule.controller.formatLiftWeight(wendler.liftSchedule.currentShowingMax,values.percentage)]}</span></p>'
         }
     ],
+    listeners:{
+        beforeshow:wendler.liftSchedule.controller.setupLiftCompleteToggle
+    },
     dockedItems:[
         {
             xtype:'toolbar',
@@ -107,9 +160,24 @@ wendler.liftSchedule.liftTemplate = {
                 {
                     text:'Back',
                     ui:'back',
-                    handler:function () {
-                        Ext.getCmp('lift-schedule').setActiveItem(Ext.getCmp('lift-selector'), {type:'slide', direction:'right'});
-                    }
+                    handler:wendler.liftSchedule.controller.returnToLiftSelect
+                },
+                {xtype:'spacer'},
+                {
+                    hidden:true,
+                    id:'mark-lift-completed-button',
+                    iconCls:'done',
+                    iconMask:true,
+                    ui:'action',
+                    handler:wendler.liftSchedule.controller.markLiftCompleted
+                },
+                {
+                    hidden:true,
+                    id:'unmark-lift-completed-button',
+                    iconCls:'done',
+                    iconMask:true,
+                    ui:'confirm',
+                    handler:wendler.liftSchedule.controller.unmarkLiftCompleted
                 }
             ]
         }
