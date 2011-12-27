970a68162f662a0d61f651879afc460b4265424e
skendall <skendall@skendall-dell.(none)>
2011-12-27 14:02:10 -0500

    Removed inconsistent "controller" refactoring.

--- js/views/settings/settings.js
+++ js/views/settings/settings.js
@@ -1,5 +1,19 @@
 "use strict";
-Ext.ns('wendler.views');
+Ext.ns('wendler.views', 'wendler.settings.controller');
+wendler.settings.controller.resetToDefaults = function() {
+    wendler.stores.Settings.first().set(wendler.defaults.settings);
+    wendler.stores.Settings.sync();
+    Ext.getCmp('settings-form').load(Ext.ModelMgr.create(wendler.defaults.settings, 'Settings'));
+};
+
+wendler.settings.controller.setLiftPercentages = function(){
+    Ext.getCmp('settings').setActiveItem(Ext.getCmp('edit-percentages-lift-schedule'));
+};
+
+wendler.settings.controller.reloadForm = function() {
+    Ext.getCmp('settings-form').load(wendler.stores.Settings.first());
+    Ext.getCmp('settings-form').hasBeenLoaded = true;
+};
 
 new Ext.form.FormPanel({
     title: 'Settings',
