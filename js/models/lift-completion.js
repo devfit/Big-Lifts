faf340fd110139d986d569c6b09da3a0d548049d
skendall <skendall@skendalllaptop.(none)>
2012-02-17 00:12:01 -0500

    Cleaned up model separation. Add a way to save and load stores to file.

---
+++ js/models/lift-completion.js
@@ -0,0 +1,53 @@
+"use strict";
+Ext.ns('wendler.stores.lifts', 'wendler.defaults');
+Ext.ns('wendler.stores.migrations');
+
+wendler.stores.lifts.findLiftCompletionByPropertyAndWeek = function (liftPropertyName, week) {
+    var completionIndex = wendler.stores.lifts.LiftCompletion.findBy(function (r) {
+        return r.get('liftPropertyName') === liftPropertyName && r.get('week') === week;
+    });
+    return wendler.stores.lifts.LiftCompletion.getAt(completionIndex);
+};
+
+wendler.stores.migrations.liftCompletionMigration = function () {
+    util.withNoFilters(wendler.stores.lifts.LiftCompletion, function () {
+        for (var i = 0; i < wendler.stores.lifts.Lifts.getCount(); i++) {
+            var liftPropertyName = wendler.stores.lifts.Lifts.getAt(i).get('propertyName');
+
+            var existingLiftCompletion = wendler.stores.lifts.LiftCompletion.findBy(function (r) {
+                return r.get('liftPropertyName') === liftPropertyName;
+            });
+
+            if (existingLiftCompletion === -1) {
+                for (var week = 1; week <= 4; week++) {
+                    wendler.stores.lifts.LiftCompletion.add(
+                        {liftPropertyName:liftPropertyName, week:week, completed:false});
+                }
+            }
+        }
+        wendler.stores.lifts.LiftCompletion.sync();
+    });
+};
+
+
+Ext.regModel('LiftCompletion', {
+    fields:[
+        {name:'id', type:'integer'},
+        {name:'liftPropertyName', type:'string'},
+        {name:'week', type:'integer'},
+        {name:'completed', type:'boolean'}
+    ],
+    proxy:{
+        type:'localstorage',
+        id:'lift-completion-proxy'
+    }
+});
+wendler.stores.lifts.LiftCompletion = new Ext.data.Store({
+    model:'LiftCompletion',
+    listeners:{
+        load:function () {
+            wendler.stores.migrations.liftCompletionMigration();
+        }
+    }
+});
+wendler.stores.lifts.LiftCompletion.load();
---
+++ js/util/store-file-backer.js
@@ -0,0 +1,22 @@
+Ext.ns('util.filebackup');
+util.filebackup.saveStore = function (store) {
+    var data = Ext.encode(Ext.pluck(store.data.items, 'data'));
+    util.files.write(util.filebackup.generateFileName(store), data);
+};
+
+util.filebackup.loadStore = function (store) {
+    util.withNoFilters(store, function () {
+        util.files.read(util.filebackup.generateFileName(store), function (data) {
+            store.removeAll();
+            var storeData = JSON.parse(data);
+            for (var i = 0; i < storeData.length; i++) {
+                store.add(storeData[i]);
+            }
+            store.sync();
+        });
+    });
+};
+
+util.filebackup.generateFileName = function (store) {
+    return store.getProxy().id + ".txt";
+};
