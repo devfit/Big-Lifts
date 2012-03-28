f792aa6b399f1974a047a9a04ec4179e9592b39f
Stefan Kendall <stefankendall@gmail.com>
2012-03-27 21:51:34 -0400

    Fixed some obvious breakage in resetting back button. Attempting once again to get android to be quittable.

--- js/util/navigation.js
+++ js/util/navigation.js
@@ -3,13 +3,11 @@ Ext.ns('wendler.navigation');
 
 wendler.navigation.resetBack = function () {
     document.removeEventListener("backbutton", wendler.navigation.back, false);
-    wendler.navigation.back = function () {};
 };
 
 wendler.navigation.back = function () {
 };
 wendler.navigation.setBackFunction = function (backFunction) {
-    document.removeEventListener("backbutton", wendler.navigation.back, false);
     wendler.navigation.back = backFunction;
     document.addEventListener('backbutton', wendler.navigation.back, false);
 };
