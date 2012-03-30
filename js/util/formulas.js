c52ccbce54ae4edaec98061708be8ea565a1da5b
Stefan Kendall <stefankendall@gmail.com>
2012-03-29 22:21:14 -0400

    Made bar formula account for existing plates. Needs to be refactored.

--- js/util/formulas.js
+++ js/util/formulas.js
@@ -6,27 +6,54 @@ util.formulas.estimateOneRepMax = function (weight, reps) {
     return util.roundNumber(estimate, '0.5', 'normal');
 };
 
-util.formulas.buildPlateListForWeight = function (targetWeight, barWeight) {
-    var availablePlates = [45, 35, 25, 15, 10, 5, 2.5, 1];
-
-    if (targetWeight < barWeight) {
-        return []
-    }
-
-    targetWeight -= barWeight;
-    var plates = [];
-    while (targetWeight > 0) {
-        var plate = _.find(availablePlates, function (p) {
-            return 2 * p <= targetWeight;
-        });
-
-        plates.push(plate);
-        targetWeight -= 2 * plate;
-    }
-
-    return plates;
-};
-
 util.formulas.lbsToKg = function (lbs) {
     return util.rounding.roundTo0p1(lbs / 2.2, 'down');
+};
+
+util.formulas.buildPlateListForWeight = function (targetWeight, barWeight, availablePlatePairs) {
+
+    if (typeof(availablePlatePairs) === 'undefined') {
+        var uniqueAvailablePlatesSet = [45, 35, 25, 15, 10, 5, 2.5, 1];
+
+        if (targetWeight < barWeight) {
+            return []
+        }
+
+        targetWeight -= barWeight;
+        var plates = [];
+        while (targetWeight > 0) {
+            var plate = _.find(uniqueAvailablePlatesSet, function (p) {
+                return 2 * p <= targetWeight;
+            });
+
+            plates.push(plate);
+            targetWeight -= 2 * plate;
+        }
+
+        return plates;
+    }
+    else {
+        uniqueAvailablePlatesSet = _.uniq(availablePlatePairs);
+
+        if (targetWeight < barWeight) {
+            return []
+        }
+
+        targetWeight -= barWeight;
+        plates = [];
+        while (targetWeight > 0) {
+            var plate = _.find(uniqueAvailablePlatesSet, function (p, index) {
+                return 2 * p <= targetWeight;
+            });
+
+            var plateIndex = _.indexOf(availablePlatePairs, plate);
+            availablePlatePairs.splice(plateIndex, 1);
+            uniqueAvailablePlatesSet = _.uniq(availablePlatePairs);
+
+            plates.push(plate);
+            targetWeight -= 2 * plate;
+        }
+
+        return plates;
+    }
 };
