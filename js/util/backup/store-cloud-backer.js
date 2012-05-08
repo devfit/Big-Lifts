ede78619a476ad3943c8c7a7321e5a7c86ba8209
Stefan Kendall <stefankendall@gmail.com>
2012-05-07 23:03:31 -0400

    Added detection of changed existing records

--- js/util/backup/store-cloud-backer.js
+++ js/util/backup/store-cloud-backer.js
@@ -58,18 +58,63 @@ util.cloudbackup.syncStoreStoresToCloud = function () {
 
 util.cloudbackup.saveStore = function (store, entireStoreSavedCallback) {
     var className = util.proxy.getProxyNameFromStore(store);
-    var saveRecordTasks = _.map(store.getRange(), function(model){
-       return function(callback){
-           parse.saveRecordForUser(util.cloudbackup.getUserIdByDeviceId(), className, model.data, callback);
-       };
+    var existingData = util.cloudbackup.cloudData[className];
+    var newRecordIds = util.cloudbackup.getNewRecordIds(existingData, store);
+    var existingRecordIds = util.cloudbackup.getExistingRecordIds(existingData, store);
+
+    var recordsToUpdate = util.cloudbackup.findChangedRecords(existingData, store, existingRecordIds);
+
+    var createRecordTasks = _.map(newRecordIds, function (id) {
+        return function (callback) {
+            var model = store.findRecord('id', id);
+            parse.saveRecordForUser(util.cloudbackup.getUserIdByDeviceId(), className, model.data, callback);
+        };
     });
 
-    async.series(saveRecordTasks, entireStoreSavedCallback);
+    async.series(createRecordTasks, entireStoreSavedCallback);
+};
+
+util.cloudbackup.getNewRecordIds = function (existingCloudData, store) {
+    var cloudIds = _.map(existingCloudData, function (data) {
+        return data.id;
+    });
+    var currentIds = _.map(store.getRange(), function (model) {
+        return model.data.id;
+    });
+    return _.difference(currentIds, cloudIds);
+};
+
+util.cloudbackup.getExistingRecordIds = function (existingCloudData, store) {
+    var cloudIds = _.map(existingCloudData, function (data) {
+        return data.id;
+    });
+    var currentIds = _.map(store.getRange(), function (model) {
+        return model.data.id;
+    });
+    return _.intersection(currentIds, cloudIds);
+};
+
+util.cloudbackup.findChangedRecords = function (existingData, store, existingRecordIds) {
+    var existingRecordsById = {};
+    _.each(existingData, function (data) {
+        existingRecordsById[data.id] = data;
+    });
+
+    return _.filter(existingRecordIds, function (existingId) {
+        var cloudRecord = existingRecordsById[existingId];
+        var currentRecord = store.findRecord('id', existingId).data;
+
+        return !_.isEqual(cloudRecord, currentRecord);
+    });
 };
 
 util.cloudbackup.getUserIdByDeviceId = function () {
     if (!_.isUndefined(device)) {
         return device.uuid;
     }
+};
+
+util.cloudbackup.getFieldsFromStore = function (store) {
+    return _.pluck(store.getModel().getFields().all, '_name');
 };
 
--- spec/javascripts/CloudBackupSpec.js
+++ spec/javascripts/CloudBackupSpec.js
@@ -1,5 +1,105 @@
 describe("CloudBackup", function () {
-    it("should provide unique starting ", function () {
-        expect(util.cloudbackup.getUserIdByDeviceId()).toEqual('1234');
+    var store;
+
+    beforeEach(function () {
+        Ext.define('TestModel', {
+            extend:'Ext.data.Model',
+            config:{
+                fields:[
+                    {name:'id', type:'integer'}
+                ],
+                proxy:{
+                    type:'localstorage',
+                    id:'meta-proxy'
+                }
+            }
+        });
+
+        store = Ext.create('Ext.data.Store', {
+            autoLoad:true,
+            model:'TestModel'
+        });
+        store.add({id:1});
+        store.add({id:2});
+        store.add({id:3});
+        store.sync();
+    });
+
+    it("should identify new record ids", function () {
+        var newRecordIds = util.cloudbackup.getNewRecordIds([
+            {id:1},
+            {id:2}
+        ], store);
+        expect(newRecordIds).toEqual([3]);
+    });
+
+    it("should identify existing record ids", function () {
+        var newRecordIds = util.cloudbackup.getExistingRecordIds([
+            {id:1},
+            {id:2}
+        ], store);
+        expect(newRecordIds).toEqual([1, 2]);
+    });
+
+    it("should get store field names", function () {
+        Ext.define('TestModel', {
+            extend:'Ext.data.Model',
+            config:{
+                fields:[
+                    {name:'id', type:'integer'},
+                    {name:'test', type:'boolean'},
+                    {name:'test2', type:'string'}
+                ],
+                proxy:{
+                    type:'localstorage',
+                    id:'meta-proxy'
+                }
+            }
+        });
+
+        var store = Ext.create('Ext.data.Store', {
+            model:'TestModel'
+        });
+
+        expect(util.cloudbackup.getFieldsFromStore(store)).toEqual(['id', 'test', 'test2']);
+    });
+
+    it("should find changed records", function () {
+        Ext.define('TestModel2', {
+            extend:'Ext.data.Model',
+            config:{
+                fields:[
+                    {name:'id', type:'integer'},
+                    {name:'name', type:'string'}
+                ],
+                proxy:{
+                    type:'localstorage',
+                    id:'changed-records-proxy'
+                }
+            }
+        });
+
+        store = Ext.create('Ext.data.Store', {
+            autoLoad:true,
+            model:'TestModel2'
+        });
+        store.add({id:1, name:"A"});
+        store.add({id:2, name:"B"});
+        store.add({id:3, name:"C"});
+        store.sync();
+
+        var oneChange = [
+            {id:1, name:"Q"},
+            {id:2, name:"B"}
+        ];
+
+        var noChanges = [
+            {id:1, name:'A'},
+            {id:2, name:'B'}
+        ];
+
+        expect(util.cloudbackup.findChangedRecords(oneChange, store, [1, 2])).toEqual([1]);
+        expect(util.cloudbackup.findChangedRecords(noChanges, store, [1, 2])).toEqual([]);
+
     });
 });
