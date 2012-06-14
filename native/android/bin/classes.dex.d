0eb3e456443240be86980aa8e4450c5ccc04b553
Stefan Kendall <stefankendall@gmail.com>
2012-06-13 23:30:41 -0400

    Added native setup for android

---
+++ native/android/bin/classes.dex.d
@@ -0,0 +1,11 @@
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes.dex : \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/R$layout.class \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/R.class \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/R$string.class \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/R$xml.class \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/BuildConfig.class \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/cordovaExample.class \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/R$drawable.class \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes/org/apache/cordova/example/R$attr.class \
+/tools/android/tools/support/annotations.jar \
+/home/skendall/work/Wendler-5-3-1/native/android/libs/cordova-1.8.1.jar \
