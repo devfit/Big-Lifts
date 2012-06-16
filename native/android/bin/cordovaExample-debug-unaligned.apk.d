59fc2d85cdb81f597a7b81c5d3ced25484592c64
Stefan Kendall <stefankendall@gmail.com>
2012-06-16 14:43:14 -0400

    Revert "Removed unnecessary files."
    This reverts commit 10655c49f423edad0eaa34dab555586d21400a33.

---
+++ native/android/bin/cordovaExample-debug-unaligned.apk.d
@@ -0,0 +1,6 @@
+/home/skendall/work/Wendler-5-3-1/native/android/bin/cordovaExample-debug-unaligned.apk : \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/cordovaExample.ap_ \
+/home/skendall/work/Wendler-5-3-1/native/android/bin/classes.dex \
+/home/skendall/work/Wendler-5-3-1/native/android/src/org/apache/cordova/example/cordovaExample.java-e \
+/tools/android/tools/support/annotations.jar \
+/home/skendall/work/Wendler-5-3-1/native/android/libs/cordova-1.8.1.jar \
