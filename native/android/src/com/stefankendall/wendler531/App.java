f1cbe5fa6c07ce8b39c48967be301674722a8cb5
Stefan Kendall <stefankendall@gmail.com>
2012-06-25 12:39:23 -0400

    Removed copies of phonegap build config file. Added flag to keep the screen on while the app is on.

--- native/android/src/com/stefankendall/wendler531/App.java
+++ native/android/src/com/stefankendall/wendler531/App.java
@@ -2,9 +2,8 @@ package com.stefankendall.wendler531;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import org.apache.cordova.*;
-
-import java.lang.Readable;
 
 public class App extends DroidGap {
     @Override
@@ -15,6 +14,7 @@ public class App extends DroidGap {
 
         int TEN_SECONDS = 10000;
         super.loadUrl("file:///android_asset/www/index.html", TEN_SECONDS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
 
