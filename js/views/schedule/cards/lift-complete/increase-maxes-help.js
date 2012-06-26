bbbf5a2a10d5bc4aca8d8b2bb5c36d00a2ee116d
Stefan Kendall <stefankendall@gmail.com>
2012-06-25 22:19:42 -0400

    Made maxes increasable from the increase maxes help page.

--- js/views/schedule/cards/lift-complete/increase-maxes-help.js
+++ js/views/schedule/cards/lift-complete/increase-maxes-help.js
@@ -1,5 +1,5 @@
 "use strict";
-Ext.ns('wendler.liftSchedule.controller', 'wendler.views.liftSchedule');
+Ext.ns('wendler.liftSchedule.controller', 'wendler.views.liftSchedule', 'wendler.liftSchedule.increaseMaxes');
 
 wendler.liftSchedule.controller.showIncreaseMaxesHelpScreen = function () {
     Ext.getCmp('lift-schedule').setActiveItem(Ext.getCmp('increase-maxes-help'),
@@ -11,6 +11,24 @@ wendler.liftSchedule.controller.closeIncreaseMaxesHelpScreen = function () {
         {type:'slide', direction:'left'});
 };
 
+wendler.liftSchedule.increaseMaxes.bindInputElements = function(){
+    _.each(Ext.getCmp('increase-maxes-help').element.query('input'), function (input) {
+        Ext.get(input).addListener('blur', wendler.liftSchedule.increaseMaxes.updateCycleIncrease);
+    })
+};
+
+wendler.liftSchedule.increaseMaxes.updateCycleIncrease = function (changedInput) {
+    var name = changedInput.target.name;
+    var value = parseInt(changedInput.target.value);
+    var liftRecord = wendler.stores.lifts.Lifts.findRecord('name', name);
+    liftRecord.set('cycleIncrease', value);
+    liftRecord.save();
+
+    Ext.getCmp('increase-maxes-display-list').refresh();
+    wendler.liftSchedule.increaseMaxes.bindInputElements();
+};
+
+
 wendler.views.liftSchedule.IncreaseMaxesHelp = {
     id:'increase-maxes-help',
     xtype:'panel',
@@ -18,6 +36,9 @@ wendler.views.liftSchedule.IncreaseMaxesHelp = {
     listeners:{
         show:function () {
             wendler.navigation.setBackFunction(wendler.liftSchedule.controller.closeIncreaseMaxesHelpScreen);
+        },
+        initialize:function () {
+            wendler.liftSchedule.increaseMaxes.bindInputElements();
         }
     },
     items:[
@@ -35,11 +56,12 @@ wendler.views.liftSchedule.IncreaseMaxesHelp = {
             ]
         },
         {
+            id:'increase-maxes-display-list',
             xtype:'list',
             store:wendler.stores.lifts.Lifts,
             itemTpl:'<table style="width:100%"><tbody><tr>' +
                 '<td style="width:33%">{name}</td>' +
-                '<td style="width:33%;color:green">+{cycleIncrease}</td>' +
+                '<td style="width:33%"><input style="color:green; width:50px" name="{propertyName}" type="number" value="{cycleIncrease}"/></td>' +
                 '<td style="width:34%">{max} → {[values.max+values.cycleIncrease]}</td>' +
                 '</tr></tbody></table>'
         }
