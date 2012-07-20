e55f4a27687b14bf8223abea99c49d9ba76fef9f
Stefan Kendall <stefankendall@gmail.com>
2012-07-20 13:23:03 -0400

    Blocking file loading for broken phone setups

--- js/util/backup/setup_file_or_local_loading.js
+++ js/util/backup/setup_file_or_local_loading.js
@@ -1,4 +1,8 @@
 Ext.ns('wendler');
 if (localStorage.length === 0) {
+    var isBrokenTouchWiz = _.isDefined(PhoneGap) && _.isUndefined(window.requestFileSystem);
     wendler.loadingFromFile = true;
+    if( !isBrokenTouchWiz ){
+        wendler.loadingFromFile = false;
+    }
 }
