5c32caa444a1015562b6ed7ce575dddb8fe146b3
Stefan Kendall <stefankendall@gmail.com>
2012-07-20 14:09:01 -0400

    Fixed debug Desktop check.

--- js/util/backup/setup_file_or_local_loading.js
+++ js/util/backup/setup_file_or_local_loading.js
@@ -1,5 +1,5 @@
 Ext.ns('wendler');
-wendler.isBrokenTouchWiz = _.isDefined(PhoneGap) && _.isUndefined(window.requestFileSystem);
+wendler.isBrokenTouchWiz = typeof(PhoneGap) !== 'undefined' && _.isUndefined(window.requestFileSystem);
 if (localStorage.length === 0) {
     wendler.loadingFromFile = true;
     if( !wendler.isBrokenTouchWiz ){
