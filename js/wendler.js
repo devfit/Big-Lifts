e1f229aff8aa5a73ff27d1544543d91a54264318
Stefan Kendall <stefankendall@gmail.com>
2012-08-12 15:19:49 -0400

    Re-arranging startup process to mitigate whitescreen issue.

--- js/wendler.js
+++ js/wendler.js
@@ -12,37 +12,41 @@ wendler.main.markFirstStartup = function () {
 
 wendler.main.started = false;
 wendler.main.start = function () {
-    if (wendler.main.started) {
-        return;
-    }
-
     wendler.main.started = true;
-
-    var startTab = wendler.stores.Meta.first().data.firstTimeInApp ? 1 : 0;
-    wendler.main.markFirstStartup();
-    wendler.main.deviceReady = true;
-
-    Ext.application({
-        launch:function () {
-            Ext.create('Ext.tab.Panel', {
-                    id:'main-tab-panel',
-                    fullscreen:true,
-                    sortable:false,
-                    tabBar:{
-                        id:'tab-navigation',
-                        docked:'bottom',
-                        layout:{ pack:'center', align:'center' }
-                    },
-                    activeItem:startTab,
-                    items:[
-                        Ext.create('Wendler.views.LiftSchedule'),
-                        Ext.create('Wendler.views.Maxes'),
-                        Ext.create('Wendler.views.Log'),
-                        Ext.create('Wendler.views.OneRepMaxCalculator'),
-                        Ext.create('Wendler.views.More')
-                    ]
-                }
-            );
-        }
-    });
+    wendler.main.loadApplication();
 };
+
+wendler.main.loadApplication = function () {
+    if (wendler.main.started && wendler.main.deviceReady) {
+        wendler.main.markFirstStartup();
+
+        var app = Ext.getCmp('main-tab-panel');
+        app.add(Ext.create('Wendler.views.LiftSchedule'));
+        app.add(Ext.create('Wendler.views.Maxes'));
+        app.add(Ext.create('Wendler.views.Log'));
+        app.add(Ext.create('Wendler.views.OneRepMaxCalculator'));
+        app.add(Ext.create('Wendler.views.More'));
+
+        app.setActiveItem(wendler.stores.Meta.first().data.firstTimeInApp ? 1 : 0);
+    }
+};
+
+Ext.application({
+    launch:function () {
+        Ext.create('Ext.tab.Panel', {
+                id:'main-tab-panel',
+                fullscreen:true,
+                sortable:false,
+                tabBar:{
+                    id:'tab-navigation',
+                    docked:'bottom',
+                    layout:{ pack:'center', align:'center' }
+                },
+                items:[
+                ]
+            }
+        );
+        wendler.main.deviceReady = true;
+        wendler.main.loadApplication();
+    }
+});
