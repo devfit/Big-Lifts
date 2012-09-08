4a4bd1b7661d7e98253fb41ac233ffe07806562e
Stefan Kendall <stefankendall@gmail.com>
2012-09-08 19:32:45 -0400

    Set tab default tab back to normal. Toggled off premium. Added test for displaying of bbb notes.

--- cucumber/features/step_definition/boring-but-big.rb
+++ cucumber/features/step_definition/boring-but-big.rb
@@ -62,6 +62,7 @@ When /^I tap (\w+) log entry (\d+)$/ do |assistance_type, row_entry|
   sleep @ANIMATION_DELAY
 end
 
-Then /^The assistance details notes shows "([^"]*?)"$/ do
-
+Then /^The assistance details notes shows "([^"]*?)"$/ do |notes|
+  log_notes = @driver.find_element(:id => 'edit-assistance-log-notes').text()
+  log_notes.should == notes
 end
