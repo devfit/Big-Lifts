5895a4bebd98baf4dbb63d81b26c508c75d3d934
Stefan Kendall <stefankendall@gmail.com>
2012-09-12 20:44:36 -0400

    Added deload indicator.

--- js/views/track/cards/log-list.js
+++ js/views/track/cards/log-list.js
@@ -192,6 +192,10 @@ wendler.logList.getWeightDisplay = function (weight) {
     return (weight == 0 || weight == null) ? "[?]" : weight;
 };
 
+wendler.logList.deloadMarker = function (week) {
+    return week === 4 ? "[D]" : "";
+}
+
 wendler.stores.Settings.addListener('beforesync', function () {
     if (wendler.main.started) {
         Ext.getCmp('lift-log-list').refresh();
@@ -325,8 +329,10 @@ wendler.views.log.cards.LogList = {
                     itemCls:'lift-log-row',
                     emptyText:'<div id="lift-log-empty-text">To track a lift, use the checkmark in the 5/3/1 view</div>',
                     itemTpl:'<table><tbody><tr>' +
-                        '<td width="30%"><div class="lift-name">{liftName}</div><div class="cycle-and-week">C{cycle} W{week}</div></td>' +
-                        '<td width="30%"><div><span class="reps">{reps}x</span> <span class="weight">{weight}</span></div>' +
+                        '<td width="32%"><div class="lift-name">{liftName}</div><div class="cycle-and-week">C{cycle} W{week} ' +
+                        '<span class="deload-indicator">{[wendler.logList.deloadMarker(values.week)]}</span>' +
+                        '</div></td>' +
+                        '<td width="28%"><div><span class="reps">{reps}x</span> <span class="weight">{weight}</span></div>' +
                         '<div class="estimated-one-rep">~{[util.formulas.estimateOneRepMax(values.weight,values.reps)]}</div></td>' +
                         '<td width="40%" class="date-week">' +
                         '<span class="date">{[wendler.log.formatDate(values.timestamp)]}</span><span class="disclosure-small"></span>' +
