0b8bfdc1bd390e7b9ad6def64d26d2789b7db36b
Stefan Kendall <stefankendall@gmail.com>
2012-10-05 23:57:12 -0400

    Removed unused file and method.

--- js/util/reset.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Ext.ns('util.data');
-util.data.hardReset = function () {
-    util.filebackup.deleteAllStoreFiles();
-    setTimeout(function () {
-        localStorage.clear();
-        window.location.reload();
-    }, 500);
-};
