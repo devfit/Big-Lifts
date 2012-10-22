e7eb056eb17344890dd53e0465060b2d0dd466b1
Stefan Kendall <stefankendall@gmail.com>
2012-10-22 15:04:51 -0400

    Revert "Fixed export url and data save directory."
    This reverts commit 4a22a2eaf4c6a0bbcc8bed9e4bcedfe28e4c05af.

--- js/views/track/cards/export.js
+++ js/views/track/cards/export.js
@@ -74,7 +74,7 @@ biglifts.log.emailExport.ajaxEmailRequest = function (email, data) {
     });
     console.log( data );
     Ext.Ajax.request({
-        url:'http://wendler.herokuapp.com/email',
+        url:'http://biglifts.herokuapp.com/email',
         method:'POST',
         params:{
             email:email,
