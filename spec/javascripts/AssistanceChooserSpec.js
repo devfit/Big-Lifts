cdb3a1afc8d45c8393300232e9a98eec42df4a48
Stefan Kendall <stefankendall@gmail.com>
2012-11-01 09:49:19 -0400

    Fixed assistance chooser specs

--- spec/javascripts/AssistanceChooserSpec.js
+++ spec/javascripts/AssistanceChooserSpec.js
@@ -1,12 +1,16 @@
 describe("Assistance Chooser", function () {
+    beforeEach(function(){
+       this.assistanceChooser = Ext.create('biglifts.views.AssistanceChooser');
+    });
+
     it("should determine last used assistance type to be null if no log entries", function () {
-        var type = biglifts.views.liftSchedule.assistance.getLastAssistanceType();
+        var type = this.assistanceChooser.getLastAssistanceType();
         expect(type).toEqual(null);
     });
 
     it("should determine last used assistance type from log entries", function () {
         biglifts.stores.assistance.ActivityLog.add({assistanceType:"BBB", timestamp: 10});
-        var type = biglifts.views.liftSchedule.assistance.getLastAssistanceType();
+        var type = this.assistanceChooser.getLastAssistanceType();
         expect(type).toEqual('BBB');
     });
 });
