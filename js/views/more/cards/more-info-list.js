e02aadca69cc818aa11a4ef44540419f4425f7c0
Stefan Kendall <stefankendall@gmail.com>
2012-11-08 09:32:33 -0700

    Fixed routine display.

--- js/views/more/cards/more-info-list.js
+++ js/views/more/cards/more-info-list.js
@@ -45,17 +45,22 @@ biglifts.more.hardReset = function () {
     });
 };
 
+var listIndex = 0;
 biglifts.more.listItems = [
-    {model:{text:'<span class="text">Routine: <span id="routine-text"></span></span><span class="disclosure"></span>'}, handler:biglifts.more.selectRoutine}
+    {model:{index:listIndex++, text:null}, handler:biglifts.more.selectRoutine, textFn:function () {
+        return '<span class="text">Routine: <span id="routine-text">' +
+            biglifts.stores.Routine.first().get("name") +
+            '</span></span><span class="disclosure"></span>';
+    }}
 ];
 
 if (biglifts.toggles.Assistance) {
-    biglifts.more.listItems.push({model:{text:'<span class="text">1RM Calculator</span><span class="disclosure"></span>'}, handler:biglifts.more.showOneRepCalculator});
+    biglifts.more.listItems.push({model:{index:listIndex++, text:'<span class="text">1RM Calculator</span><span class="disclosure"></span>'}, handler:biglifts.more.showOneRepCalculator});
 }
 
-biglifts.more.listItems.push({model:{text:'<span class="text">Settings</span><span class="disclosure"></span>'}, handler:biglifts.more.showSettings});
-biglifts.more.listItems.push({model:{text:'<span class="text">Feedback...</span><span class="disclosure"></span>'}, handler:biglifts.more.feedback});
-biglifts.more.listItems.push({model:{text:'<span class="text">Reset</span><span class="warning"></span>'}, handler:biglifts.more.hardReset});
+biglifts.more.listItems.push({model:{index:listIndex++, text:'<span class="text">Settings</span><span class="disclosure"></span>'}, handler:biglifts.more.showSettings});
+biglifts.more.listItems.push({model:{index:listIndex++, text:'<span class="text">Feedback...</span><span class="disclosure"></span>'}, handler:biglifts.more.feedback});
+biglifts.more.listItems.push({model:{index:listIndex++, text:'<span class="text">Reset</span><span class="warning"></span>'}, handler:biglifts.more.hardReset});
 
 biglifts.more.listData = [];
 for (var i = 0; i < biglifts.more.listItems.length; i++) {
@@ -66,6 +71,7 @@ Ext.define('MoreList', {
     extend:'Ext.data.Model',
     config:{
         fields:[
+            {name:'index', type:'int'},
             {name:'text', type:'string'}
         ]
     }
@@ -75,6 +81,11 @@ biglifts.more.listStore = Ext.create('Ext.data.Store', {
     data:biglifts.more.listData
 });
 
+biglifts.more.getTextForValues = function (values) {
+    var textFn = biglifts.more.listItems[values.index].textFn;
+    return values.text ? values.text : textFn();
+};
+
 biglifts.views.MoreInfoList = {
     xtype:'panel',
     id:'more-info-list-panel',
@@ -82,12 +93,6 @@ biglifts.views.MoreInfoList = {
     listeners:{
         show:function () {
             biglifts.navigation.unbindBackEvent();
-        },
-        painted:function () {
-            util.withLoadedStore(biglifts.stores.Routine, function () {
-                var routine = biglifts.stores.Routine.first().get("name");
-                Ext.get('routine-text').setHtml(routine);
-            });
         }
     },
     items:[
@@ -99,7 +104,7 @@ biglifts.views.MoreInfoList = {
         {
             id:'more-info-list',
             xtype:'list',
-            itemTpl:'{text}',
+            itemTpl:'{[biglifts.more.getTextForValues(values)]}',
             itemCls:'more-info-row',
             store:biglifts.more.listStore,
             listeners:{
