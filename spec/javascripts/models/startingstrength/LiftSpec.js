e1121c4ecba566d43f3057418d0c271a4515f6a7
Stefan Kendall <stefankendall@gmail.com>
2012-11-23 19:23:05 -0500

    Setup workout model for ss.

--- spec/javascripts/models/startingstrength/LiftSpec.js
+++ spec/javascripts/models/startingstrength/LiftSpec.js
@@ -1,4 +1,4 @@
-describe("Starting Strength lift spec", function () {
+describe("Starting Strength lift", function () {
     var liftStore = biglifts.stores.ss.Lifts;
     it("should load default lifts", function () {
         liftStore.load();
---
+++ spec/javascripts/models/startingstrength/WorkoutSpec.js
@@ -0,0 +1,17 @@
+describe("Starting Strength workout", function () {
+    beforeEach(function () {
+        this.workoutStore = Ext.create('biglifts.models.startingstrength.WorkoutStore');
+        this.workoutStore.removeAll();
+        this.workoutStore.sync();
+    });
+
+    it("should load default workouts", function () {
+        expect(this.workoutStore.getCount()).toEqual(0);
+        this.workoutStore.load();
+        expect(this.workoutStore.getCount()).toEqual(6);
+        this.workoutStore.filter('name', 'A');
+        expect(this.workoutStore.getCount()).toEqual(3);
+        this.workoutStore.filter('name','B');
+        expect(this.workoutStore.getCount()).toEqual(3);
+    });
+});
