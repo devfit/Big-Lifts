f16603304c2fda9a9ee7c7a02a2fe0bb3df09630
Stefan Kendall <stefankendall@gmail.com>
2012-11-30 21:04:01 -0500

    Cleaned up spec.

--- spec/javascripts/models/LigtLogSpec.js
+++ spec/javascripts/models/LigtLogSpec.js
@@ -1,21 +1,22 @@
 describe("The lift log store", function () {
     beforeEach(function () {
-        biglifts.stores.LiftLog.removeAll();
+        localStorage.clear();
+        this.liftLog = biglifts.stores.LiftLog;
     });
 
     it("should return sort by date secondarily if sorting A-Z", function () {
-        biglifts.stores.LiftLog.add({liftName:'Squat', timestamp:1000});
-        biglifts.stores.LiftLog.add({liftName:'Squat', timestamp:3000});
-        biglifts.stores.LiftLog.add({liftName:'Squat', timestamp:4000});
-        biglifts.stores.LiftLog.add({liftName:'Squat', timestamp:2000});
-        biglifts.stores.LiftLog.add({liftName:'Press', timestamp:1500});
+        this.liftLog.add({liftName:'Squat', timestamp:1000});
+        this.liftLog.add({liftName:'Squat', timestamp:3000});
+        this.liftLog.add({liftName:'Squat', timestamp:4000});
+        this.liftLog.add({liftName:'Squat', timestamp:2000});
+        this.liftLog.add({liftName:'Press', timestamp:1500});
 
-        biglifts.stores.LiftLog.sortLog('liftName', 'ASC');
+        this.liftLog.sortLog('liftName', 'ASC');
 
-        expect(biglifts.stores.LiftLog.getAt(0).get('liftName')).toEqual("Press");
-        expect(biglifts.stores.LiftLog.getAt(1).get('timestamp')).toEqual(4000);
-        expect(biglifts.stores.LiftLog.getAt(2).get('timestamp')).toEqual(3000);
-        expect(biglifts.stores.LiftLog.getAt(3).get('timestamp')).toEqual(2000);
-        expect(biglifts.stores.LiftLog.getAt(4).get('timestamp')).toEqual(1000);
+        expect(this.liftLog.getAt(0).get('liftName')).toEqual("Press");
+        expect(this.liftLog.getAt(1).get('timestamp')).toEqual(4000);
+        expect(this.liftLog.getAt(2).get('timestamp')).toEqual(3000);
+        expect(this.liftLog.getAt(3).get('timestamp')).toEqual(2000);
+        expect(this.liftLog.getAt(4).get('timestamp')).toEqual(1000);
     });
 });
