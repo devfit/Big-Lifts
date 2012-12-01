ea60610cac25f49e773676b1791443e73a77c2e7
Stefan Kendall <stefankendall@gmail.com>
2012-12-01 14:26:02 -0500

    Added failing test to prove no incrementing is happening.

--- cucumber/features/startingstrength/track/tracking-lifts.feature
+++ cucumber/features/startingstrength/track/tracking-lifts.feature
@@ -1,5 +1,23 @@
 @StartingStrength
 Feature: Tracking Lifts
+  Scenario: Weights increase between sessions
+    When I navigate to the "Edit" tab
+    When I set the "Bench" input to "225"
+    When I set the "Squat" input to "300"
+    When I set the "Deadlift" input to "400"
+    When I set the "Power Clean" input to "200"
+    When I set the "Press" input to "120"
+    And I navigate to the "Lift" tab
+    And I tap the checkmark
+    And I navigate to the "Lift" tab
+    And I tap the "B" tab
+    And I tap the checkmark
+    And I navigate to the "Edit" tab
+    Then The "Bench" input is "230"
+    Then The "Squat" input is "320"
+    Then The "Deadlift" input is "405"
+    Then The "Power Clean" input is "205"
+    Then The "Press" input is "125"
 
   Scenario: Tracking lifts
     When I navigate to the "Edit" tab
--- cucumber/features/step_definition/startingstrength/edit/edit-lifts.rb
+++ cucumber/features/step_definition/startingstrength/edit/edit-lifts.rb
@@ -1,9 +1,19 @@
+def find_input_by_label label
+  label = @driver.find_elements(:class => 'x-form-label').select { |l| l.text().include? label }[0]
+  return label.find_element(:xpath => '..').find_element(:tag_name => 'input')
+end
+
 When /^I set the "(.*?)" input to "(.*?)"$/ do |name, weight|
-  label = @driver.find_elements(:class => 'x-form-label').select { |label| label.text().include? name }[0]
-  input = label.find_element(:xpath => '..').find_element(:tag_name => 'input')
+  input = find_input_by_label name
   input.clear
   input.send_keys weight
 end
+
+Then /^The "(.*?)" input is "(.*?)"$/ do |input_name, value|
+  input = find_input_by_label input_name
+  input.attribute('value').should == value
+end
+
 
 Then /^The Starting Strength "(.*?)" lift is "(.*?)"$/ do |name, expected_weight|
   actual_weight = @driver.execute_script("return biglifts.stores.ss.Lifts.findRecord('name','#{name}').get('weight')")
