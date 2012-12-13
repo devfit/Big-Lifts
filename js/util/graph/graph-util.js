bca9b8b580078ef091a4424db215d84eae3efe0d
Stefan Kendall <stefankendall@gmail.com>
2012-12-12 22:20:48 -0500

    Fixed re-viewing a chart being completely broken. Removed deload week from chart.

--- js/util/graph/graph-util.js
+++ js/util/graph/graph-util.js
@@ -4,22 +4,24 @@ biglifts.util.graph.convertLogToGraphStore = function () {
     var data = [];
     biglifts.stores.LiftLog.sort('timestamp', 'ASC');
     biglifts.stores.LiftLog.each(function (r) {
-        biglifts.util.graph.addLogRecordToData(data, r);
+        if (r.get('week') !== 4) {
+            biglifts.util.graph.addLogRecordToData(data, r);
+        }
     });
 
     return Ext.create('Ext.data.JsonStore', {
-        fields:_.flatten([
+        fields: _.flatten([
             'date',
             biglifts.stores.lifts.Lifts.getUniqueLiftNames()
         ]),
-        data:data
+        data: data
     });
 };
 
 biglifts.util.graph.addLogRecordToData = function (data, record) {
     var formattedDate = new Date(record.get('timestamp'));
     var graphRecord = {
-        date:formattedDate
+        date: formattedDate
     };
     graphRecord[record.get('liftName')] = util.formulas.estimateOneRepMax(record.get('weight'), record.get("reps"));
 
--- js/views/track/cards/graph.js
+++ js/views/track/cards/graph.js
@@ -10,72 +10,73 @@ Ext.define('biglifts.LiftGraph', {
     xtype: 'liftgraph',
     _setupGraph: function () {
         if (Ext.getCmp('lift-chart')) {
-            Ext.getCmp('lift-chart').destroy();
+            Ext.getCmp('lift-chart').bindStore(biglifts.util.graph.convertLogToGraphStore());
         }
-
-        var series = [];
-        _.each(biglifts.stores.lifts.Lifts.getUniqueLiftNames(), function (lift) {
-            series.push({
-                type: 'line',
-                xField: 'date',
-                yField: lift,
-                axis: 'left',
-                highlight: true,
-                showInLegend: true
+        else {
+            var series = [];
+            _.each(biglifts.stores.lifts.Lifts.getUniqueLiftNames(), function (lift) {
+                series.push({
+                    type: 'line',
+                    xField: 'date',
+                    yField: lift,
+                    axis: 'left',
+                    highlight: true,
+                    showInLegend: true
+                });
             });
-        });
 
-        Ext.getCmp('graph').add({
-            id: 'lift-chart',
-            xtype: 'chart',
-            store: biglifts.util.graph.convertLogToGraphStore(),
-            animate: true,
-            shadow: false,
-            legend: {
-                position: {
-                    portrait: 'bottom',
-                    landscape: 'bottom'
-                }
-            },
-            axes: [
-                {
-                    type: 'Numeric',
-                    position: 'left',
-                    fields: biglifts.stores.lifts.Lifts.getUniqueLiftNames(),
-                    label: {
-                        renderer: function (v) {
-                            return v.toFixed(1);
-                        }
-                    },
-                    title: 'Est. 1RM',
-                    grid: {
-                        odd: {
-                            opacity: 1,
-                            stroke: '#aaa',
-                            'stroke-width': 1
-                        }
+            Ext.getCmp('graph').add({
+                id: 'lift-chart',
+                xtype: 'chart',
+                store: biglifts.util.graph.convertLogToGraphStore(),
+                animate: true,
+                shadow: false,
+                legend: {
+                    position: {
+                        portrait: 'bottom',
+                        landscape: 'bottom'
                     }
                 },
-                {
-                    type: 'Category',
-                    position: 'bottom',
-                    label: {
-                        renderer: function (date) {
-                            var dateFormat = biglifts.stores.w.Settings.first().get('dateFormat');
-                            var datePieces = dateFormat.split("/");
-                            var format = datePieces[0] + "/" + datePieces[1];
-                            return date.toString(format);
+                axes: [
+                    {
+                        type: 'Numeric',
+                        position: 'left',
+                        fields: biglifts.stores.lifts.Lifts.getUniqueLiftNames(),
+                        label: {
+                            renderer: function (v) {
+                                return v.toFixed(1);
+                            }
                         },
-                        rotate: {
-                            degrees: 315
+                        title: 'Est. 1RM',
+                        grid: {
+                            odd: {
+                                opacity: 1,
+                                stroke: '#aaa',
+                                'stroke-width': 1
+                            }
                         }
                     },
-                    fields: ['date'],
-                    title: 'Date'
-                }
-            ],
-            series: series
-        });
+                    {
+                        type: 'Category',
+                        position: 'bottom',
+                        label: {
+                            renderer: function (date) {
+                                var dateFormat = biglifts.stores.w.Settings.first().get('dateFormat');
+                                var datePieces = dateFormat.split("/");
+                                var format = datePieces[0] + "/" + datePieces[1];
+                                return date.toString(format);
+                            },
+                            rotate: {
+                                degrees: 315
+                            }
+                        },
+                        fields: ['date'],
+                        title: 'Date'
+                    }
+                ],
+                series: series
+            });
+        }
     },
     config: {
         layout: 'fit',
