aa30ea5966a64aa7657d87e2b0bf039afa97eca3
Stefan Kendall <stefankendall@gmail.com>
2012-12-18 22:44:56 -0500

    Fixed training maxes test

--- features/step_definition/training_maxes.rb
+++ features/step_definition/training_maxes.rb
@@ -1,9 +1,13 @@
+def get_training_maxes
+  @driver.find_elements(:class => 'x-docking-vertical').select { |d| d.displayed? && d.text().include?("90%") }[0]
+end
+
 Then /^Training maxes are visible$/ do
-  @driver.find_element(:id => 'training-maxes').displayed?().should be_true
+  get_training_maxes().should_not == nil
 end
 
 Then /^Training maxes are not visible$/ do
-  @driver.find_element(:id => 'training-maxes').displayed?().should be_false
+  get_training_maxes().should == nil
 end
 
 Then /^The training max for clean is shown$/ do
