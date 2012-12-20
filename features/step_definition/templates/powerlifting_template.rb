bb818952c6183b4c70e7f82f9c3d2041f6fb0ac2
Stefan Kendall <stefankendall@gmail.com>
2012-12-19 21:23:44 -0500

    Fixed powerlifting template test

--- features/step_definition/templates/powerlifting_template.rb
+++ features/step_definition/templates/powerlifting_template.rb
@@ -1,9 +1,14 @@
+def get_meet_goals_fieldset
+  @driver.find_elements(:class => 'x-form-fieldset-title').select { |m| m.displayed? && m.text().include?('Meet Goals') }[0]
+end
+
 When /^I set the (\w+) meet goal to (\d+)$/ do |lift, weight|
-  lift_input = @driver.find_element(:id => 'meet-goals').find_element(:name => lift)
+  form = get_meet_goals_fieldset().find_element(:xpath => '../..')
+  lift_input = form.find_element(:name => lift)
   lift_input.clear
   lift_input.send_keys weight
 end
 
 Then /^Meet goals are not visible$/ do
-  @driver.find_element(:id => 'meet-goals').should_not be_displayed
+  get_meet_goals_fieldset().should == nil
 end
