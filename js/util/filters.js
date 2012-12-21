88e5e91746b0db54cce47aad3ce4d6b6968cae87
Stefan Kendall <stefankendall@gmail.com>
2012-12-20 22:41:27 -0500

    Removed filter cleanup after withNoFilters.

--- js/util/filters.js
+++ js/util/filters.js
@@ -4,14 +4,11 @@ util.withNoFilters = function (store, callback) {
     var storeFilters = store.getFilters();
     if (storeFilters.length === 0) {
         callback(store);
-        store.clearFilter();
     }
     else {
         var filters = _.clone(storeFilters);
         store.clearFilter(true);
         callback(store);
-        //ignoring the event here throws exceptions
-        store.clearFilter();
         _.each(filters, function (f) {
             store.filter(f);
         });
