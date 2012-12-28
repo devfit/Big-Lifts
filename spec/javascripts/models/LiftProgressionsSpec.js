4979b022a06c486a85a46f8ef2efa820d331f81c
Stefan Kendall <stefankendall@gmail.com>
2012-12-27 19:04:12 -0500

    Removed dead specs.

--- spec/javascripts/models/LiftProgressionsSpec.js
+++ spec/javascripts/models/LiftProgressionsSpec.js
@@ -1,39 +1,4 @@
 describe("Lift Progressions", function () {
-    it("should set the amrap property properly during migration", function () {
-        var store = Ext.create('biglifts.stores.LiftProgressions');
-
-        store.add({week: 1, set: 1, percentage: 50, reps: 5});
-        store.add({week: 1, set: 2, percentage: 50, reps: 5});
-        store.add({week: 1, set: 3, percentage: 50, reps: 5});
-        store.add({week: 1, set: 4, percentage: 50, reps: 5});
-        store.add({week: 1, set: 5, percentage: 50, reps: 5});
-        store.add({week: 1, set: 6, percentage: 50, reps: 5});
-        store.add({week: 2, set: 1, percentage: 50, reps: 5});
-
-        store.setupAmrapForSixthSet(store);
-        expect(store.getAt(0).get('amrap')).toEqual(false);
-        expect(store.getAt(5).get('amrap')).toEqual(true);
-        expect(store.getAt(6).get('amrap')).toEqual(false);
-    });
-
-    it("should set the warmup property properly during migration", function () {
-        var store = Ext.create('biglifts.stores.LiftProgressions');
-
-        store.add({week: 1, set: 1, percentage: 50, reps: 5});
-        store.add({week: 1, set: 2, percentage: 50, reps: 5});
-        store.add({week: 1, set: 3, percentage: 50, reps: 5});
-        store.add({week: 1, set: 4, percentage: 50, reps: 5});
-        store.add({week: 1, set: 5, percentage: 50, reps: 5});
-        store.add({week: 1, set: 6, percentage: 50, reps: 5});
-        store.add({week: 2, set: 1, percentage: 50, reps: 5});
-
-        store.setupWarmupForFirstThreeSets(store);
-        expect(store.getAt(0).get('warmup')).toEqual(true);
-        expect(store.getAt(2).get('warmup')).toEqual(true);
-        expect(store.getAt(5).get('warmup')).toEqual(false);
-        expect(store.getAt(6).get('warmup')).toEqual(true);
-    });
-
     it("should stitch together sets of a progression after removal", function () {
         var store = Ext.create('biglifts.stores.LiftProgressions');
 
