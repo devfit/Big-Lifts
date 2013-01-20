a9e2872638cfb7ecf9e0e2a013a63c5432a6ae29
Stefan Kendall <stefankendall@gmail.com>
2013-01-19 23:35:27 -0500

    Restricted rate popup to iOS users.

--- js/models/migration/migrations/009_rate_app.js
+++ js/models/migration/migrations/009_rate_app.js
@@ -1,15 +1,17 @@
 Ext.define('biglifts.migrations.RateApp', {
     run:function () {
         util.withLoadedStore(biglifts.stores.Routine, function () {
-            if (biglifts.stores.Routine.getCount() !== 0) {
-                util.whenApplicationReady(function () {
-                    Ext.Msg.confirm('Rate the App!', 'If you like the app, please rate it!', function (text) {
-                        if (text === "yes") {
-                            var appId = biglifts.premium ? "534996988" : "455470293";
-                            location.href = "itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=" + appId;
-                        }
+            if (Ext.os.is.iOS) {
+                if (biglifts.stores.Routine.getCount() !== 0) {
+                    util.whenApplicationReady(function () {
+                        Ext.Msg.confirm('Rate the App!', 'If you like the app, please rate it!', function (text) {
+                            if (text === "yes") {
+                                var appId = biglifts.premium ? "534996988" : "455470293";
+                                location.href = "itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=" + appId;
+                            }
+                        });
                     });
-                });
+                }
             }
         });
     }
