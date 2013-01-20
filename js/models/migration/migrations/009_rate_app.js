f11391cf825cd49406ed8047ba70942e4e483831
Stefan Kendall <stefankendall@gmail.com>
2013-01-19 23:16:14 -0500

    Blocked review for only users who have existing installs.

--- js/models/migration/migrations/009_rate_app.js
+++ js/models/migration/migrations/009_rate_app.js
@@ -1,12 +1,16 @@
 Ext.define('biglifts.migrations.RateApp', {
     run:function () {
-        util.whenApplicationReady(function () {
-            Ext.Msg.confirm('Rate the App!', 'If you like the app, please rate it!', function (text) {
-                if (text === "yes") {
-                    var appId = biglifts.premium ? "534996988" : "455470293";
-                    location.href = "itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=" + appId;
-                }
-            });
+        util.withLoadedStore(biglifts.stores.Routine, function () {
+            if (biglifts.stores.Routine.getCount() !== 0) {
+                util.whenApplicationReady(function () {
+                    Ext.Msg.confirm('Rate the App!', 'If you like the app, please rate it!', function (text) {
+                        if (text === "yes") {
+                            var appId = biglifts.premium ? "534996988" : "455470293";
+                            location.href = "itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=" + appId;
+                        }
+                    });
+                });
+            }
         });
     }
 });
