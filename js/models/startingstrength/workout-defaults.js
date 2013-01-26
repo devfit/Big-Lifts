25e340eb9b018300693a3188b2d1bc976e82c83d
Stefan Kendall <stefankendall@gmail.com>
2013-01-26 17:32:22 -0500

    Fixing deadlift defaults

--- js/models/startingstrength/workout-defaults.js
+++ js/models/startingstrength/workout-defaults.js
@@ -24,7 +24,7 @@ biglifts.models.startingstrength.workouts.standard = {
         {sets:2, reps:5, name:'A', percentage:40, warmup:true, order:0},
         {sets:1, reps:3, name:'A', percentage:60, warmup:true, order:1},
         {sets:1, reps:2, name:'A', percentage:85, warmup:true, order:2},
-        {sets:1, reps:2, name:'A', percentage:100, warmup:false, order:3}
+        {sets:1, reps:5, name:'A', percentage:100, warmup:false, order:3}
     ],
     press:[
         {sets:2, reps:5, name:'B', percentage:0, warmup:true, order:0},
@@ -73,10 +73,10 @@ biglifts.models.startingstrength.workouts.novice = {
         {sets:2, reps:5, name:'A', percentage:40, warmup:true, order:0},
         {sets:1, reps:3, name:'A', percentage:60, warmup:true, order:1},
         {sets:1, reps:2, name:'A', percentage:85, warmup:true, order:2},
-        {sets:1, reps:2, name:'A', percentage:100, warmup:false, order:3},
+        {sets:1, reps:5, name:'A', percentage:100, warmup:false, order:3},
         {sets:2, reps:5, name:'B', percentage:40, warmup:true, order:0},
         {sets:1, reps:3, name:'B', percentage:60, warmup:true, order:1},
         {sets:1, reps:2, name:'B', percentage:85, warmup:true, order:2},
-        {sets:1, reps:2, name:'B', percentage:100, warmup:false, order:3}
+        {sets:1, reps:5, name:'B', percentage:100, warmup:false, order:3}
     ]
 };
