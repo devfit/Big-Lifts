102c92c2a58199d6cc40a557ba5ee492f626806c
Stefan Kendall <stefankendall@gmail.com>
2013-01-29 21:35:03 -0500

    Fixed step definitions for reps to beat

--- features/step_definition/reps-to-beat.rb
+++ features/step_definition/reps-to-beat.rb
@@ -1,7 +1,7 @@
 Then /^The best estimated one rep max is ([\d\.]+)$/ do |estimatedOneRep|
-  @driver.find_element(:id => 'last-one-rep-estimate').text.should == estimatedOneRep
+  @driver.find_element(:class => 'reps-toolbar').text().should include estimatedOneRep
 end
 
 Then /^The reps to beat is (\d+)$/ do |reps|
-  @driver.find_element(:id => 'reps-needed-to-beat-last-estimate').text.should == reps
+  @driver.find_element(:class => 'reps-toolbar').text().should include "Reps to beat: #{reps}"
 end
