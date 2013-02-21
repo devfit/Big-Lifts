7f003d5f5cbae4c899079f682ef1fe1eb9266a32
Stefan Kendall <stefankendall@gmail.com>
2013-02-20 19:35:36 -0500

    Re-worded sync alert.

--- js/models/migration/migrations/015_sync_alert.js
+++ js/models/migration/migrations/015_sync_alert.js
@@ -3,7 +3,7 @@ Ext.define('biglifts.migrations.SyncAlert', {
         util.withLoadedStore(biglifts.stores.Routine, function () {
             if (biglifts.stores.Routine.getCount() !== 0) {
                 util.whenApplicationReady(function () {
-                    Ext.Msg.confirm('Sync Available!', 'Would you like to set your username/password now?', function (text) {
+                    Ext.Msg.confirm('5/3/1 Sync Available!', 'Would you like to set your username/password now?', function (text) {
                         if (text === "yes") {
                             Ext.getCmp('app').setActiveItem(Ext.getCmp('setup'));
                             Ext.getCmp('setup').setActiveItem(Ext.getCmp('user-setup'));
