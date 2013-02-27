9ce21784cffa2abbbd0f032b77210364344cd39e
Stefan Kendall <stefankendall@gmail.com>
2013-02-26 21:07:51 -0500

    Working on removing carousel

--- js/views/schedule/cards/lift-settings/lift-settings.js
+++ js/views/schedule/cards/lift-settings/lift-settings.js
@@ -1,22 +1,58 @@
-Ext.ns('biglifts.views.liftSchedule', 'biglifts.liftSettings', 'biglifts.liftProgressions');
+Ext.define('biglifts.views.LiftSettings', {
+    extend: 'Ext.Panel',
+    returnToLiftSelectFromSettings: function () {
+        Ext.getCmp('lift-schedule').setActiveItem(Ext.getCmp('lift-selector'));
+    },
+    carouselBack: function () {
+        var liftSettings = Ext.getCmp('lift-settings');
+        liftSettings.setActiveItem(liftSettings.getActiveIndex() - 1);
+    },
+    carouselForward: function () {
+        var liftSettings = Ext.getCmp('lift-settings');
+        liftSettings.setActiveItem(liftSettings.getActiveIndex() + 1);
+    },
+    setupLiftScheme: function (scheme) {
+        var prebuiltVariation = biglifts.liftProgressions.options[scheme];
+        biglifts.stores.lifts.LiftProgression.clearFilter();
+        biglifts.stores.lifts.LiftProgression.removeAll();
+        biglifts.stores.lifts.LiftProgression.add(prebuiltVariation);
+        biglifts.stores.lifts.LiftProgression.sync();
 
-biglifts.views.liftSchedule.LiftSettings = {
-    id:'lift-settings',
-    xtype:'carousel',
-    listeners:{
-        painted:function () {
-            biglifts.navigation.setBackFunction(biglifts.liftSettings.returnToLiftSelectFromSettings);
-        }
+        var template = biglifts.stores.Template.first();
+        template.set('name', scheme);
+        template.set('hasMeetGoals', scheme === "powerlifting");
+        biglifts.stores.Template.sync();
+
+        biglifts.stores.WeekRotation.removeAll();
+        biglifts.stores.WeekRotation.sync();
+
+        biglifts.stores.lifts.MeetGoals.removeAll();
+        biglifts.stores.lifts.syncMeetGoalsToLifts();
+
+        var customMessages = {'powerlifting': 'Meet goals have been defaulted to your 1RM estimates. You can change meet goals on the lifts tab.'};
+        var message = _.has(customMessages, scheme) ? customMessages[scheme] : 'The lift scheme has been updated';
+
+        Ext.Msg.alert('Lifts Updated', message, Ext.emptyFn);
     },
-    defaults:{
-        xtype:'formpanel',
-        scroll:'vertical'
-    },
-    items:[
-        biglifts.liftSettings.templates.fresher,
-        biglifts.liftSettings.templates.heavier,
-        biglifts.liftSettings.templates.powerlifting,
-        biglifts.liftSettings.templates.rotating,
-        biglifts.liftSettings.templates.custom
-    ]
-};
+    config: {
+        id: 'lift-settings',
+        layout: 'card',
+        activeItem: 0,
+        listeners: {
+            painted: function () {
+                biglifts.navigation.setBackFunction(biglifts.liftSettings.returnToLiftSelectFromSettings);
+            }
+        },
+        defaults: {
+            xtype: 'formpanel',
+            scroll: 'vertical'
+        },
+        items: [
+            biglifts.liftSettings.templates.fresher,
+            biglifts.liftSettings.templates.heavier,
+            biglifts.liftSettings.templates.powerlifting,
+            biglifts.liftSettings.templates.rotating,
+            biglifts.liftSettings.templates.custom
+        ]
+    }
+});
--- js/views/schedule/cards/lift-settings/templates/custom-template.js
+++ js/views/schedule/cards/lift-settings/templates/custom-template.js
@@ -1,40 +1,40 @@
-Ext.ns('biglifts.liftSettings.templates');
-biglifts.liftSettings.showEditLiftPercentages = function () {
-    Ext.getCmp('lift-schedule').setActiveItem(Ext.getCmp('edit-lift-percentages'));
-};
-
-biglifts.liftSettings.templates.custom = {
-    padding:5,
-    items:[
-        {
-            xtype:'toolbar',
-            docked:'top',
-            title:"Custom",
-            items:[
-                {
-                    text:'Back',
-                    ui:'back',
-                    handler:biglifts.liftSettings.carouselBack
-                }
-            ]
-        },
-        {
-            xtype:'toolbar',
-            docked:'top',
-            ui:'light',
-            items:[
-                {xtype:'spacer'},
-                {
-                    xtype:'button',
-                    ui:'confirm',
-                    text:'Use',
-                    handler:biglifts.liftSettings.showEditLiftPercentages
-                }
-            ]
-        },
-        {
-            html:'Setup sets, reps, and percentages manually',
-            margin:"0 0 5 0"
-        }
-    ]
-};
+Ext.define('biglifts.views.templates.Custom', {
+    showEditLiftPercentages: function () {
+        Ext.getCmp('lift-schedule').setActiveItem(Ext.getCmp('edit-lift-percentages'));
+    },
+    config: {
+        padding: 5,
+        items: [
+            {
+                xtype: 'toolbar',
+                docked: 'top',
+                title: "Custom",
+                items: [
+                    {
+                        text: 'Back',
+                        ui: 'back',
+                        handler: biglifts.liftSettings.carouselBack
+                    }
+                ]
+            },
+            {
+                xtype: 'toolbar',
+                docked: 'top',
+                ui: 'light',
+                items: [
+                    {xtype: 'spacer'},
+                    {
+                        xtype: 'button',
+                        ui: 'confirm',
+                        text: 'Use',
+                        handler: biglifts.liftSettings.showEditLiftPercentages
+                    }
+                ]
+            },
+            {
+                html: 'Setup sets, reps, and percentages manually',
+                margin: "0 0 5 0"
+            }
+        ]
+    }
+});
