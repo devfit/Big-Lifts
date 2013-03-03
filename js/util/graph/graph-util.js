c91963860cad9f440d5f96a1d88f597e7a533a6b
Stefan Kendall <stefankendall@gmail.com>
2013-03-02 20:01:18 -0500

    Sorted log by time to make graph correct

--- js/util/graph/graph-util.js
+++ js/util/graph/graph-util.js
@@ -6,11 +6,11 @@ biglifts.util.graph.convertLogToGraphStore = function (lift_id) {
         biglifts.stores.lifts.Lifts.findRecord('id', lift_id).get('name');
 
     return Ext.create('Ext.data.JsonStore', {
-        fields:_.flatten([
+        fields: _.flatten([
             'date',
             graphLifts
         ]),
-        data:data
+        data: data
     });
 };
 
@@ -21,11 +21,16 @@ biglifts.util.graph.buildData = function (lift_id) {
     }
 
     var data = [];
-    biglifts.stores.LiftLog.each(function (r) {
-        if (r.get('week') !== 4) {
-            biglifts.util.graph.addLogRecordToData(data, r);
-        }
+    util.withNoSorters(biglifts.stores.LiftLog, function () {
+        biglifts.stores.LiftLog.sort('timestamp', 'ASC');
+        biglifts.stores.LiftLog.each(function (r) {
+            if (r.get('week') !== 4) {
+                biglifts.util.graph.addLogRecordToData(data, r);
+            }
+        });
+        biglifts.stores.LiftLog.setSorters([]);
     });
+
 
     biglifts.stores.LiftLog.clearFilter();
     return data;
@@ -34,7 +39,7 @@ biglifts.util.graph.buildData = function (lift_id) {
 biglifts.util.graph.addLogRecordToData = function (data, record) {
     var formattedDate = new Date(record.get('timestamp'));
     var graphRecord = {
-        date:formattedDate
+        date: formattedDate
     };
     graphRecord[record.get('liftName')] = util.formulas.estimateOneRepMax(record.get('weight'), record.get("reps"));
     biglifts.util.graph.extendOrAdd(data, graphRecord);
