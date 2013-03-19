04b7e158741d0c2a688523926bf8232e88601ad6
Stefan Kendall <stefankendall@gmail.com>
2013-03-19 19:31:08 -0400

    Fixed deadlift default increase for starting strength.

--- js/models/startingstrength/lift-defaults.js
+++ js/models/startingstrength/lift-defaults.js
@@ -4,14 +4,14 @@ biglifts.models.startingstrength.lifts.standard = {
     lbs:[
         {name:'Squat', weight:200, increase:10},
         {name:'Bench', weight:135, increase:5},
-        {name:'Deadlift', weight:225, increase:5},
+        {name:'Deadlift', weight:225, increase:10},
         {name:'Press', weight:100, increase:5},
         {name:'Power Clean', weight:135, increase:5}
     ],
     kg:[
         {name:'Squat', weight:100, increase:5},
         {name:'Bench', weight:70, increase:2},
-        {name:'Deadlift', weight:110, increase:2},
+        {name:'Deadlift', weight:110, increase:5},
         {name:'Press', weight:50, increase:2},
         {name:'Power Clean', weight:70, increase:2}
     ]
@@ -21,13 +21,13 @@ biglifts.models.startingstrength.lifts.novice = {
     lbs:[
         {name:'Squat', weight:200, increase:10},
         {name:'Bench', weight:135, increase:5},
-        {name:'Deadlift', weight:225, increase:5},
+        {name:'Deadlift', weight:225, increase:10},
         {name:'Press', weight:100, increase:5}
     ],
     kg:[
         {name:'Squat', weight:100, increase:5},
         {name:'Bench', weight:70, increase:2},
-        {name:'Deadlift', weight:110, increase:2},
+        {name:'Deadlift', weight:110, increase:5},
         {name:'Press', weight:50, increase:2}
     ]
 };
