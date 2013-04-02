0e1cdfafad1f7444334991af058791275d296e85
Stefan Kendall <stefankendall@gmail.com>
2013-04-02 08:17:10 -0400

    Rebuilt ss lifts to allow workout restore to work

--- js/models/migration/migrations/019_rebuild_ss_ordering.js
+++ js/models/migration/migrations/019_rebuild_ss_ordering.js
@@ -6,6 +6,10 @@ Ext.define('biglifts.migrations.RebuildSsWorkouts', {
             }
 
             util.withLoadedStore(biglifts.stores.ss.WorkoutStore, function () {
+                biglifts.stores.ss.Lifts.removeAll();
+                biglifts.stores.ss.Lifts.sync();
+                Ext.create('biglifts.models.startingstrength.TemplateSwitcher').setupLifts('standard');
+
                 biglifts.stores.ss.WorkoutStore.removeAll();
                 biglifts.stores.ss.WorkoutStore.sync();
                 biglifts.stores.ss.WorkoutStore.rebuildWorkouts();
